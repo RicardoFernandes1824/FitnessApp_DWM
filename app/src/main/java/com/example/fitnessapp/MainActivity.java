38d8415c877bffd623a1a17045614b1f91b2dce6
Ricardo Fernandes <ricardo.fernandes@ipvc.pt>
2024-12-16 17:42:08 +0000

    first commit

---
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.fitnessapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fitnessapp.databinding.MainActivityBinding;
+
+public class MainActivity extends AppCompatActivity {
+    MainActivityBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = MainActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginbtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MainActivity2.class)));
+    }
+}
