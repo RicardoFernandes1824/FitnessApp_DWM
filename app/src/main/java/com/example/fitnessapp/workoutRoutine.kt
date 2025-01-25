d8c6d7be29da8b954ce3583a9f227572800c2b96
Ricardo Fernandes <ricardo.fernandes@ipvc.pt>
2025-01-25 17:47:15 +0000

    New Screens

---
+++ app/src/main/java/com/example/fitnessapp/workoutRoutine.kt
@@ -0,0 +1,11 @@
+package com.example.fitnessapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WorkoutRoutine : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.workout_routine_screen)
+    }
+}
